1c1fcd597ffaffbf9e77b40325f6f9d2809708e8
jrg <1209309223@qq.com>
2017-08-28 11:59:09 +0800

    Create diskfree.c

---
+++ diskfree.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include “server.h”
+/* HTML source for the start of the page we generate. */
+static char* page_start =
+“<html>\n”
+“ <body>\n”
+“ <pre>\n”;
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include “server.h”
+/* HTML source for the start of the page we generate. */
+static char* page_start =
+“<html>\n”
+“ <body>\n”
+“ <pre>\n”;
+}
